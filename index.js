3fdf840e09c7349ae83e104f4279892f41b6239e
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-01 12:48:41 +0500

    nodemon installed

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const book = [
 ]
 app.get('/list',(req,res)=>{
     res.send(book)
+    console.log(req.query, "this is query")
+    console.log(req.params, "params")
+    console.log(req.headers , "headers")
+    console.log(req.body, "body")
 })
 app.put('/update',(req,res)=>{
     res.send("This is put request")
@@ -21,3 +25,15 @@ app.put('/update',(req,res)=>{
 app.post('/add',(req,res)=>{
     res.send("This is post request")
 })
+
+app.delete('/delete',(req,res)=>{   
+     
+    res.send("This is delete request");
+}
+)
+
+app.put('/put',(req, res) => {
+    // console.log("put request")
+    res.send("This is put request")
+}
+)    
